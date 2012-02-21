c209fc5988a7dbb989292ea7ac2fe651017eb09e
bleen <git.119x@bleen.net>
2012-02-21 17:04:02 -0500

    Adding a global settings form for DFP

--- plugins/export_ui/dfp_tag_ui.class.php
+++ plugins/export_ui/dfp_tag_ui.class.php
@@ -11,26 +11,10 @@
 class dfp_tag_ui extends ctools_export_ui {
 
   /**
-   * Prepare the item object before the edit form is rendered.
-   */
-  function edit_form(&$form, &$form_state) {
-    $form_state['item']->settings = unserialize($form_state['item']->settings);
-
-    parent::edit_form($form, $form_state);
-  }
-
-  /**
    * Prepare the tag values before they are added to the database.
    */
   function edit_form_submit(&$form, &$form_state) {
-    $settings = unserialize($form_state['item']->settings);
-
-    // Remove empty targeting values before storing them in the database.
-    foreach ($form_state['values']['targeting'] as $key => $target) {
-      if (empty($target['target']) && empty($target['value'])) {
-        unset($form_state['values']['targeting'][$key]);
-      }
-    }
+    $settings = $form_state['item']->settings;
 
     $settings['targeting'] = $form_state['values']['targeting'];
     $settings['slug'] = $form_state['values']['slug'];
