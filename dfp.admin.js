2068b378cfa9aea429a513fc5ea2e90ecd9e6b23
bleen <git.119x@bleen.net>
2012-02-23 18:07:07 -0500

    Adding descritpions to vertical tab forms on global settings page

--- dfp.admin.js
+++ dfp.admin.js
@@ -5,15 +5,46 @@
  */
 Drupal.behaviors.dfpVerticalTabs = {
   attach: function (context) {
+
+    var checkmark = '&#10003;';
+    var exmark = '&#10008;';
+
+    $('fieldset#edit-global-tag-settings', context).drupalSetSummary(function (context) {
+      var networkId = Drupal.checkPlain($('#edit-dfp-network-id', context).val());
+      var adUnit = Drupal.checkPlain($('#edit-dfp-default-adunit', context).val());
+      var async = Drupal.checkPlain($('#edit-dfp-async-rendering', context).is(':checked'));
+      var single = Drupal.checkPlain($('#edit-dfp-single-request', context).is(':checked'));
+
+      summary = 'Network Id: ' + networkId + '<br/>';
+      summary += (async == "true" ? checkmark : exmark) + ' Load ads asyncronously' + '<br/>';
+      summary += (single == "true" ? checkmark : exmark) + ' Use a single request';
+
+      return summary;
+    });
+
+    $('fieldset#edit-global-display-options', context).drupalSetSummary(function (context) {
+      var slug = Drupal.checkPlain($('#edit-dfp-slug', context).val());
+      var noscript = Drupal.checkPlain($('#edit-dfp-use-noscript', context).is(':checked'));
+      var collapse = Drupal.checkPlain($('#edit-dfp-collapse-empty-divs', context).is(':checked'));
+
+      summary = 'Global Slug: ' + slug + '<br/>';
+      summary += (noscript == "true" ? checkmark : exmark) + ' Include <code>&lt;noscript&gt;</code> tags' + '<br/>';
+      summary += (collapse == "true" ? checkmark : exmark) + ' Hide ad slots if no ad is served';
+
+      return summary;
+    });
+
     $('fieldset#edit-tag-settings', context).drupalSetSummary(function (context) {
       var summary = Drupal.t('General configuration options');
       var name = Drupal.checkPlain($('#edit-name', context).val());
       var size = Drupal.checkPlain($('#edit-size', context).val());
       return summary + (name != '' ? '<br/>' + name  + ' [' + size + ']' : '');
     });
-    $('fieldset#edit-display-settings', context).drupalSetSummary(function (context) {
+
+    $('fieldset#edit-tag-display-options', context).drupalSetSummary(function (context) {
       return Drupal.t('Configure how the ad will be displayed');
     });
+
     $('fieldset#edit-targeting-settings', context).drupalSetSummary(function (context) {
       var summary = '';
       $('.field-target-target', context).each(function (context) {
