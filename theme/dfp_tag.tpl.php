d8025aa9ecde895e5cd4e2aea9818ce4e1ec981e
bleen <git.119x@bleen.net>
2012-02-24 17:14:38 -0500

    Theming DFP tag

--- theme/dfp_tag.tpl.php
+++ theme/dfp_tag.tpl.php
@@ -1,3 +1,10 @@
 <div <?php print drupal_attributes($wrapper_attributes) ?>>
-  <?php //print $js; ?>
+  <?php if ($tag->slug) { ?>
+    <div class="slug"><?php print $tag->slug ?></div>
+  <?php } ?>
+  <script type="text/javascript">
+    googletag.cmd.push(function() {
+      googletag.display("<?php print $tag->wrapper_id ?>");
+    });
+  </script>
 </div>
