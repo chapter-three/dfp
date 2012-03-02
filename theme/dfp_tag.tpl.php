96fc3f40d74ee55651039eecdfd1154347870b32
bleen <git.119x@bleen.net>
2012-03-02 15:56:03 -0500

    Adding the files needed to integrate with contexts module

--- theme/dfp_tag.tpl.php
+++ theme/dfp_tag.tpl.php
@@ -2,9 +2,11 @@
   <?php if ($tag->slug) { ?>
     <div class="slug"><?php print $tag->slug ?></div>
   <?php } ?>
-  <script type="text/javascript">
-    googletag.cmd.push(function() {
-      googletag.display("<?php print $tag->wrapper_id ?>");
-    });
-  </script>
+  <div <?php print drupal_attributes($placeholder_attributes) ?>>
+    <script type="text/javascript">
+      googletag.cmd.push(function() {
+        googletag.display("<?php print $tag->placeholder_id ?>");
+      });
+    </script>
+  </div>
 </div>
